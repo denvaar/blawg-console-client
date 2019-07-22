03b13224ac28dc51c918f916c1cce8724730e424
Denver Smith <denverpsmith@gmail.com>
2019-07-22 00:10:53 -0600

    Make update work

--- src/main.rs
+++ src/main.rs
@@ -13,25 +13,45 @@ use std::path::PathBuf;
 use std::process::Command;
 use structopt::StructOpt;
 use tempfile::NamedTempFile;
+use serde::{Serialize, Deserialize};
 
 type HmacSha256 = Hmac<Sha256>;
 
+const TITLE_BEGIN: &str = "████████████████████████████████████████
+██████████████   TITLE   ███████████████
+████████████████████████████████████████";
 const CONTENT_BEGIN: &str = "████████████████████████████████████████
 ██████████████  CONTENT  ███████████████
 ████████████████████████████████████████";
 const DATE_BEGIN: &str = "████████████████████████████████████████
 ██████████████   DATE    ███████████████
 ████████████████████████████████████████";
-const TAGS_BEGIN: &str = "████████████████████████████████████████
-██████████████   TAGS    ███████████████
-████████████████████████████████████████";
-const TAGS_END: &str = "\n";
+// const TAGS_BEGIN: &str = "████████████████████████████████████████
+// ██████████████   TAGS    ███████████████
+// ████████████████████████████████████████";
+const END: &str = "\n";
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Article {
+    article: ArticleData
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct ArticleData {
+    content: String,
+    title: String,
+}
 
 #[derive(Debug, StructOpt)]
-#[structopt(name = "Blawg", about = "Use plain text editor of choice to manage web content.")]
+#[structopt(
+    name = "blawg",
+    about = "Create, edit, and publish articles to the web from the safety of your favorite plain text editor."
+)]
 struct Opt {
-    #[structopt(parse(from_os_str))]
+    #[structopt(parse(from_os_str), help = "Path to a file. The content will be loaded as the content of an article.")]
     input: Option<PathBuf>,
+    #[structopt(short = "u", long = "update", help = "Update an existing article")]
+    slug: Option<String>,
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -39,7 +59,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // Check for required ENV variables
 
-    let api_create_url = match std::env::var("BLAWG_API_CREATE") {
+    let _api_create_url = match std::env::var("BLAWG_API_CREATE") {
         Ok(url) => url,
         Err(_e) => {
             eprintln!("Error: 'BLAWG_API_CREATE' environment variable undefined.");
@@ -47,19 +67,48 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
     };
 
+
     let opt = Opt::from_args();
 
-
-    // Prepare contents of temporary file
-
-    let existing_content = match opt.input {
-        Some(file_path) => std::fs::read_to_string(file_path)?,
-        None => String::from(""),
+    let slug = match opt.slug {
+        Some(slug) => slug,
+        None => String::from("")
     };
 
+    let api_update_url = match std::env::var("BLAWG_API_UPDATE") {
+        Ok(url) => url.replace("{slug}", &slug),
+        Err(_e) => {
+            eprintln!("Error: 'BLAWG_API_UPDATE' environment variable undefined.");
+            std::process::exit(1);
+        }
+    };
+
+    println!("{} {}", &slug, &api_update_url.replace("{slug}", &slug));
+
     let file = NamedTempFile::new()?;
-    let path = file.path();
-    std::fs::write(path, build_defaults(&existing_content))?;
+
+    if slug != "" {
+        let article: Article = match load_remote_content(&slug) {
+            Ok(article) => article,
+            Err(_e) => {
+                eprintln!("Error: Article matching '{}' not found.", &slug);
+                std::process::exit(1);
+            },
+        };
+
+        let path = file.path();
+        std::fs::write(path, build_defaults_from_remote(article))?;
+    } else {
+        // Prepare contents of temporary file
+
+        let existing_content = match opt.input {
+            Some(file_path) => std::fs::read_to_string(file_path)?,
+            None => String::from(""),
+        };
+
+        let path = file.path();
+        std::fs::write(path, build_defaults(&existing_content))?;
+    }
 
 
     // Spawn text editor to to open a new document
@@ -95,24 +144,43 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Hash the contents of the message and
     // send the request.
 
-    let date = request.get("date");
-    let content = request.get("content");
-    let tags = request.get("tags");
+    let date = request.get("article").expect("no article").get("date");
+    let content = request.get("article").expect("no article").get("content");
+    let title = request.get("article").expect("no article").get("title");
+    // let tags = request.get("tags");
 
-    if let (Some(date), Some(content), Some(tags)) = (date, content, tags) {
+    if content.is_none() || content == Some(&String::from("")) {
+        std::process::exit(0);
+    }
 
-        let response =
+
+    // if let (Some(date), Some(content), Some(tags)) = (date, content, tags) {
+    if let (Some(date), Some(content), Some(title)) = (date, content, title) {
+
+        let mut response =
             reqwest::Client::new()
-            .post(&api_create_url)
+            .patch(&api_update_url)
             .json(&request)
-            .header(reqwest::header::AUTHORIZATION, create_request_hmac(&date, &content, &tags))
+            // .header(reqwest::header::AUTHORIZATION, create_request_hmac(&date, &content, &tags))
+            .header(reqwest::header::AUTHORIZATION, create_request_hmac(&date, &content, &title))
             .send()?;
 
+        println!("{:?}", response);
 
         match response.status() {
             reqwest::StatusCode::OK => println!("Posted"),
             unhandled => println!("{:?}", unhandled),
         };
+
+        let errors = match response.json() {
+            Ok(r) => r,
+            Err(e) => {
+                eprintln!("error happened {:?}", e);
+                std::process::exit(1);
+            }
+        };
+
+        println!("{:?}", &errors);
     }
 
     Ok(())
@@ -133,44 +201,85 @@ fn extract(content: &String, delim: (&str, &str)) -> String {
 }
 
 fn build_defaults(existing_content: &str) -> String {
-    let default_tags = "";
+    // let default_tags = "";
+    let default_title = "";
     let now = Utc::now();
     let (_ce, year) = now.year_ce();
     let default_date = format!("{}/{}/{}", now.month(), now.day(), year);
     let default_content = format!(
-        "{begin_content}\n{content}\n\n{begin_date}\n{date}\n\n{begin_tags}\n{tags}{end_tags}",
+        "{begin_title}\n{title}\n\n{begin_content}\n{content}\n\n{begin_date}\n{date}",
         date=default_date,
-        tags=default_tags,
+        title=default_title,
         begin_content=CONTENT_BEGIN,
         content=existing_content,
         begin_date=DATE_BEGIN,
-        begin_tags=TAGS_BEGIN,
-        end_tags=TAGS_END
+        begin_title=TITLE_BEGIN
     );
 
     String::from(default_content)
 }
 
-fn create_request_payload(content_payload: &String) -> HashMap<&str, String> {
+fn build_defaults_from_remote(article: Article) -> String {
+    format!(
+        "{begin_title}\n{title}\n\n{begin_content}\n{content}\n\n{begin_date}\n{date}",
+        date=String::from(""),
+        title=article.article.title,
+        begin_content=CONTENT_BEGIN,
+        content=article.article.content,
+        begin_date=DATE_BEGIN,
+        begin_title=TITLE_BEGIN
+    )
+}
+
+fn create_request_payload(content_payload: &String) -> HashMap<&str, HashMap<&str, String>> {
     let content = extract(content_payload,
                           (CONTENT_BEGIN, DATE_BEGIN));
     let date = extract(content_payload,
-                          (DATE_BEGIN, TAGS_BEGIN));
-    let tags = extract(content_payload,
-                          (TAGS_BEGIN, TAGS_END));
+                          (DATE_BEGIN, END));
+    let title = extract(content_payload,
+                          (TITLE_BEGIN, CONTENT_BEGIN));
 
     let mut request = HashMap::new();
-    request.insert("content", content);
-    request.insert("date", date);
-    request.insert("tags", tags);
+    let mut inner = HashMap::new();
+    inner.insert("content", content);
+    inner.insert("date", date);
+    // request.insert("tags", tags);
+    inner.insert("title", title);
+
+    request.insert("article", inner);
 
     request
 }
 
-fn create_request_hmac(date: &String, content: &String, tags: &String) -> String {
-    let mut mac = HmacSha256::new_varkey(b"secret_key")
+fn create_request_hmac(date: &String, content: &String, title: &String) -> String {
+    let secret_key = match std::env::var("BLAWG_SECRET_KEY") {
+        Ok(key) => key,
+        Err(_e) => {
+            eprintln!("Error: 'BLAWG_SECRET_KEY' environment variable undefined.");
+            std::process::exit(1);
+        }
+    };
+
+    let mut mac = HmacSha256::new_varkey(secret_key.as_bytes())
         .expect("HMAC can take key of any size");
-    mac.input(format!("{}{}{}", date, content, tags).as_bytes());
+
+    let mut all_params = vec![
+      "article",
+      "content",
+      "title",
+      "date",
+      &date,
+      &content,
+      &title
+    ];
+
+    all_params.sort();
+
+    let serialized_params = all_params.join("");
+
+    println!("{}", serialized_params);
+    mac.input(serialized_params.as_bytes());
+
     let result = mac.result();
     let code_bytes = result.code();
 
@@ -181,4 +290,18 @@ fn create_request_hmac(date: &String, content: &String, tags: &String) -> String
             .map(|x| format!("{:02x}", x))
             .fold(String::new(), |acc, x| { acc + &x })
     )
+}
+
+fn load_remote_content(slug: &String) -> Result<Article, reqwest::Error> {
+    let path_var = match std::env::var("BLAWG_API_GET") {
+        Ok(p) => p,
+        Err(_e) => {
+            eprintln!("Error: 'BLAWG_API_GET' environment variable undefined.");
+            std::process::exit(1);
+        }
+    };
+    let path = format!("{}/{}", path_var, slug);
+    let response_body: Article = reqwest::get(&path)?.json()?;
+
+    Ok(response_body)
 }
